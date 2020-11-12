6d1c781d5746616142fea4c56c5f3eb1d956507a
aukrainskiy <aukrainskiy@phoenixit.ru>
2020-11-12 08:37:02 +0300

    rename migration

--- database/migrations/2020_11_03_111048_create_stat_events_table.php
+++ database/migrations/2020_11_03_111048_create_stat_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ServicesCatalogCreateStatEventsTable extends Migration
+class CreateStatEventsTable extends Migration
 {
     /**
      * Run the migrations.
